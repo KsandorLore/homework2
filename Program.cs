6ac541446ce72bbbcbf791e38865974909a5c7f5
Aleksandr Markachov <ksandorlore@gmail.com>
2024-01-23 18:21:25 +0300

    homework2

--- Program.cs
+++ Program.cs
@@ -1,90 +1,72 @@
-﻿//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
-
-/*void ABC(int num)
-{
-    if (num % 7 == 0 && num % 23 == 0)
+﻿/*
+    while(true)
     {
-        System.Console.Write("да");
-    }
-    else System.Console.Write("нет");
-}
-System.Console.WriteLine("Inpun num:");
-int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число или q для выхода:");
+        string input = Console.ReadLine();
 
-ABC(number);*/
-
-//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
-
-/*void Coordinates(int x, int y)
-{
-    if (x > 0 && y > 0)
-    {
-        Console.WriteLine("1");
-    }
-    if (x < 0 && y > 0)
-    {
-        Console.WriteLine("2");
-    }
-    if (x < 0 && y < 0)
-    {
-        Console.WriteLine("3");
-    }
-    if (x > 0 && y < 0)
-    {
-        Console.WriteLine("4");
-    }
-}
-
-System.Console.WriteLine("Inpun x coordinate:");
-int x = Convert.ToInt32(Console.ReadLine());
-
-System.Console.WriteLine("Inpun y coordinate:");
-int y = Convert.ToInt32(Console.ReadLine());
-
-Coordinates(x, y);*/
-
-//Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
-
-/*Console.WriteLine("Input num from 10 to 99:");
-int num = Convert.ToInt32(Console.ReadLine());
-
-if (num >= 10 && num <= 99)
-{
-    int ed = num % 10;
-    int dec = num / 10;
-
-    if (ed > dec)
-    {
-        Console.WriteLine(ed);
-    }
-    else Console.WriteLine(dec);
-
-}
-else Console.WriteLine("Num is not from 10 to 99");*/
-
-//Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
-
-Console.WriteLine("Input num:");
-int num = Convert.ToInt32(Console.ReadLine());
-
-if (num > 10)
-{
-    while (num > 0)
-    {
-        int dig = num % 10;
-        num = num / 10;
-        if (num > 0)
+        if (input == "q")
         {
-            Console.Write(dig + ",");
+            break;
         }
-        else
+        int num;
+        if (int.TryParse(input, out num))
         {
-            Console.Write(dig);
+            int sum = 0;
+            while (num > 0)
+            {
+                sum = num % 10;
+                num = num / 10;
+            }
+            if (sum % 2 == 0)
+            {
+                Console.WriteLine("[STOP]");
+                break;
+            }
         }
-    
+        else 
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое числоили 'q'.");
+        }
+
     }
-}
-else 
+    */
+
+
+/*  int[] num = new int[10];
+  Random rnd = new Random();
+  int EvenCount = 0;
+  for (int i = 0; i < num.Length; i++)
+  {
+      num[i] = rnd.Next(100, 1000);
+      if (num[i] % 2 == 0)
+      {
+          EvenCount++;
+      }
+
+
+  }
+
+Console.WriteLine($"\nКоличество четных чисел в массиве: {EvenCount}");
+*/
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+int temp;
+
+Console.Write("Исходный массив: ");
+foreach (int num in numbers)
 {
-    Console.WriteLine(num);
+    Console.Write(num + " ");
+}
+
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+    temp = numbers[i];
+    numbers[i] = numbers[numbers.Length - 1 - i];
+    numbers[numbers.Length - 1 - i] = temp;
+}
+
+Console.Write("\nПеревернутый массив: ");
+foreach (int num in numbers)
+{
+    Console.Write(num + " ");
 }
